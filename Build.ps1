7d51e87f7ec1654ebe898acb1690d722e63889aa
Hoàng Nguyễn <nkahoang@nkahnt.com>
2017-10-09 07:06:46 +1100

    Adding missing EnsurePsbuildInstalled

--- Build.ps1
+++ Build.ps1
@@ -1,5 +1,26 @@
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
+function EnsurePsbuildInstalled{  
+    [cmdletbinding()]
+    param(
+        [string]$psbuildInstallUri = 'https://raw.githubusercontent.com/ligershark/psbuild/master/src/GetPSBuild.ps1'
+    )
+    process{
+        if(-not (Get-Command "Invoke-MsBuild" -errorAction SilentlyContinue)){
+            'Installing psbuild from [{0}]' -f $psbuildInstallUri | Write-Verbose
+            (new-object Net.WebClient).DownloadString($psbuildInstallUri) | iex
+        }
+        else{
+            'psbuild already loaded, skipping download' | Write-Verbose
+        }
+
+        # make sure it's loaded and throw if not
+        if(-not (Get-Command "Invoke-MsBuild" -errorAction SilentlyContinue)){
+            throw ('Unable to install/load psbuild from [{0}]' -f $psbuildInstallUri)
+        }
+    }
+}
+
 EnsurePsbuildInstalled
 
 exec { & dotnet restore }
